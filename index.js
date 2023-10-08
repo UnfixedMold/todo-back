e277467af5e1fba17a5f8fa1e3a1a97fd06954a2
Henrikas <henrikasgir@gmail.com>
2023-10-08 20:47:44 +0300

    initial

---
+++ index.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+DB_URI = "mongodb+srv://henrikas:PzeR0TxNZD6glHqe@cluster0.aaqiodn.mongodb.net/todoDB"
+
+mongoose.connect(process.env.DB_URI , { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Could not connect to MongoDB', err));
+
+const todoSchema = new mongoose.Schema({
+  name: String,
+  is_checked: Boolean
+});
+
+const Todo = mongoose.model('Todo', todoSchema);
+
+// GET all todos
+app.get('/todos', async (req, res) => {
+  try {
+    const todos = await Todo.find();
+    res.json(todos);
+  } catch (err) {
+    res.status(500).json({ message: 'Server error', error: err });
+  }
+});
+
+// GET a single todo
+app.get('/todos/:id', async (req, res) => {
+  try {
+    const todo = await Todo.findById(req.params.id);
+    if (!todo) return res.status(404).json({ message: 'Todo not found' });
+    res.json(todo);
+  } catch (err) {
+    res.status(500).json({ message: 'Server error', error: err });
+  }
+});
+
+// POST a new todo
+app.post('/todos', async (req, res) => {
+  try {
+    const newTodo = new Todo({
+      name: req.body.name,
+      is_checked: req.body.is_checked
+    });
+    await newTodo.save();
+    res.json(newTodo);
+  } catch (err) {
+    res.status(400).json({ message: 'Bad request', error: err });
+  }
+});
+
+// PUT (update) a todo
+app.put('/todos/:id', async (req, res) => {
+  try {
+    const updatedTodo = await Todo.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!updatedTodo) return res.status(404).json({ message: 'Todo not found' });
+    res.json(updatedTodo);
+  } catch (err) {
+    res.status(400).json({ message: 'Bad request', error: err });
+  }
+});
+
+// DELETE a todo
+app.delete('/todos/:id', async (req, res) => {
+  try {
+    const todo = await Todo.findByIdAndDelete(req.params.id);
+    if (!todo) return res.status(404).json({ message: 'Todo not found' });
+    res.json({ message: 'Todo deleted' });
+  } catch (err) {
+    res.status(500).json({ message: 'Server error', error: err });
+  }
+});
+
+app.listen(4444, () => {
+  console.log('Server running');
+});
