ea072d9c6893d4c78ecf3f4ef8024bee20420505
Henrikas <henrikasgir@gmail.com>
2023-10-08 20:52:36 +0300

    removed db uri

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-DB_URI = "mongodb+srv://henrikas:PzeR0TxNZD6glHqe@cluster0.aaqiodn.mongodb.net/todoDB"
-
 mongoose.connect(process.env.DB_URI , { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB', err));
