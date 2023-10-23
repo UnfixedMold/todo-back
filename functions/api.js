75bf016f6b5779e1848ecd0d96ff93d972ab9ce7
Henrikas <henrikasgir@gmail.com>
2023-10-23 19:32:59 +0300

    quick fix

--- functions/api.js
+++ functions/api.js
@@ -5,6 +5,9 @@ const router = express.Router();
 const cors = require('cors');
 const mongoose = require('mongoose');
 
+app.use(cors());
+app.use(express.json());
+
 mongoose.connect(process.env.DB_URI , { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB', err));
@@ -78,7 +81,5 @@ router.delete('/todos/:id', async (req, res) => {
 
 
 app.use('/.netlify/functions/api', router);
-app.use(cors());
-app.use(express.json());
 
 module.exports.handler = serverless(app);
